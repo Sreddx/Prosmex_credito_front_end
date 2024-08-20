9800381af68c093041d4e9af7be87ca872369faa
luisma-inbest <luisma.juarez@inbest.com>
2024-08-20 13:18:35 -0600

    Updating Dashboard

--- src/renderer/Components/dashboard/Dashboard.tsx
+++ src/renderer/Components/dashboard/Dashboard.tsx
@@ -15,25 +15,86 @@ function Dashboard() {
         </button>
       </aside>
       <main className="content">
-        <h1>Dashboard general</h1>
-        {/* Aquí puedes incluir la tabla con datos */}
+        <h1>Reporte General</h1>
         <table className="data-table">
           <thead>
             <tr>
-              <th>Columna 1</th>
-              <th>Columna 2</th>
-              <th>Columna 3</th>
-              <th>Columna 4</th>
+              <th>GERENTE</th>
+              <th>SUPERVISOR</th>
+              <th>TITULAR</th>
+              <th>RUTA</th>
+              <th>GRUPO</th>
+              <th>COB IDEAL</th>
+              <th>COB REAL</th>
+              <th>PRESTAMO PAPEL</th>
+              <th>PRESTAMO REAL</th>
+              <th>MOROSIDAD $$$</th>
+              <th>MOROSIDAD %</th>
+              <th>% PRESTAMO</th>
+              <th>SOBRANTE</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>Dato 1</td>
-              <td>Dato 2</td>
-              <td>Dato 3</td>
-              <td>Dato 4</td>
+              <td>GERENTE</td>
+              <td>SUPERVISOR</td>
+              <td>TITULAR</td>
+              <td>RUTA</td>
+              <td>GRUPO 1</td>
+              <td>$2000.0</td>
+              <td>$2000.0</td>
+              <td>$5000.0</td>
+              <td>$4000.0</td>
+              <td>$0.0</td>
+              <td>0.0</td>
+              <td>2.0</td>
+              <td>-2000.0</td>
             </tr>
-            {/* Más filas de datos */}
+            <tr>
+              <td>GERENTE</td>
+              <td>SUPERVISOR</td>
+              <td>TITULAR</td>
+              <td>RUTA</td>
+              <td>GRUPO 2</td>
+              <td>$2500.0</td>
+              <td>$2300.0</td>
+              <td>$8000.0</td>
+              <td>$7000.0</td>
+              <td>$200.0</td>
+              <td>0.08</td>
+              <td>3.043478</td>
+              <td>-4700.0</td>
+            </tr>
+            <tr>
+              <td>GERENTE</td>
+              <td>SUPERVISOR</td>
+              <td>TITULAR</td>
+              <td>RUTA</td>
+              <td>GRUPO 3</td>
+              <td>$2000.0</td>
+              <td>$1800.0</td>
+              <td>$1500.0</td>
+              <td>$1000.0</td>
+              <td>$200.0</td>
+              <td>0.1</td>
+              <td>0.555556</td>
+              <td>800.0</td>
+            </tr>
+            <tr>
+              <td>TOTALES</td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>$6500.0</td>
+              <td>$6100.0</td>
+              <td>$14500.0</td>
+              <td>$12000.0</td>
+              <td>$400.0</td>
+              <td>0.061538</td>
+              <td>0.938462</td>
+              <td>-5900.0</td>
+            </tr>
           </tbody>
         </table>
       </main>
